80d3b0392257bc4625df1853155e86102885e60d
Orlando Mendoza <orlandomr27@gmail.com>
2020-06-25 21:05:22 -0600

    WIP Massive Merge Files

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import re
 
 from PyPDF2 import PdfFileMerger
 from PySide2.QtCore import Qt, Slot
@@ -47,7 +48,7 @@ class ListWidget(QListWidget):
             return super().dropEvent(event)
 
 
-class outputField(QLineEdit):
+class OutputField(QLineEdit):
     def __init__(self):
         super().__init__()
         # self.height = 55
@@ -76,23 +77,21 @@ class outputField(QLineEdit):
                 self.setText(event.mimeData().urls()[0].toLocalFile())
         else:
             event.ignore()
+
+
 # class outputField
 
 
-class button(QPushButton):
+class Button(QPushButton):
     def __init__(self, label_text):
         super().__init__()
         self.setText(label_text)
-        """
         self.setStyleSheet('''
-            font-size: 30px;
             width: 180px;
-            height: 50px;
         ''')
-        """
 
 
-class tabSinglePDFWidget(QWidget):
+class TabSinglePDFWidget(QWidget):
     def __init__(self):
         super().__init__()
 
@@ -101,22 +100,22 @@ class tabSinglePDFWidget(QWidget):
         self.buttons_layout = QHBoxLayout()
 
         # Creates and adds the QLineEdit output_file
-        self.output_file = outputField()
+        self.output_file = OutputField()
         self.output_folder_layout.addWidget(self.output_file)
 
         # Creates and adds a QPushButton to browse the output file
-        self.btn_browse = button('&Save to')
+        self.btn_browse = Button('&Save to')
         self.btn_browse.clicked.connect(self.populate_file_name)
         self.output_folder_layout.addWidget(self.btn_browse)
 
         # Creates the ListBox Widget
         self.pdf_list_widget = ListWidget()
 
-        #Create the buttons
-        self.btn_delete = button('&Delete')
-        self.btn_merge = button('&Merge')
-        self.btn_close = button('&Close')
-        self.btn_reset = button('&Reset')
+        # Create the buttons
+        self.btn_delete = Button('&Delete')
+        self.btn_merge = Button('&Merge')
+        self.btn_close = Button('&Close')
+        self.btn_reset = Button('&Reset')
 
         # Connect the buttons to the slots
         self.btn_delete.clicked.connect(self.delete_selected)
@@ -185,52 +184,114 @@ class tabSinglePDFWidget(QWidget):
                 self.dialog_message(e)
         else:
             self.dialog_message('There are no files to merge')
-# class tabSinglePDFWidget
 
-class tabMassivePDFWidget(QWidget):
+
+# class TabSinglePDFWidget
+
+
+class TabMassivePDFWidget(QWidget):
     def __init__(self):
         super().__init__()
 
         self.layout = QVBoxLayout()
-        self.output_folder_layout = QHBoxLayout()
+        self.source_folder_layout = QHBoxLayout()
         self.dest_folder_layout = QHBoxLayout()
-        self.buttons_layout = QHBoxLayout()
+        self.middle_layout = QHBoxLayout()
+        self.right_layout = QVBoxLayout()
 
-        # QLineEdit for output_folder
-        self.output_folder = outputField()
-        self.output_folder_layout.addWidget(self.output_folder)
+        # QLineEdit for output folder and source folder
+        self.source_folder = OutputField()
+        self.dest_folder = OutputField()
+        self.source_folder_layout.addWidget(self.source_folder)
+        self.dest_folder_layout.addWidget(self.dest_folder)
 
-        # Creates and adds a QPushButton to browse the output file
-        self.btn_browse = button('Save to')
-        self.btn_browse.clicked.connect(self.populate_file_name)
-        self.output_folder_layout.addWidget(self.btn_browse)
+        # Creates and adds a QPushButton to browse the source folder
+        self.btn_browse_source = Button('From folder')
+        self.btn_browse_source.clicked.connect(self.populate_source_file)
+        self.source_folder_layout.addWidget(self.btn_browse_source)
+
+        # Creates and adds a QPushButton to browse the destination folder
+        self.btn_browse_dest = Button('To folder')
+        self.btn_browse_dest.clicked.connect(self.populate_dest_file)
+        self.dest_folder_layout.addWidget(self.btn_browse_dest)
+
+        # Creates a button to load files and get the possible order of files
+        self.btn_load = Button('Load files')
+        self.btn_load.clicked.connect(self.load_files)
+        self.right_layout.addWidget(self.btn_load)
+
+        # Creates a button to merge the files in the order showed by ListWidget
+        self.btn_merge = Button('Merge')
+        self.btn_merge.clicked.connect(self.massive_merge)
+        self.right_layout.addWidget(self.btn_merge)
 
         # Creates the ListBox Widget
-        self.pdf_list_widget = ListWidget()
+        self.pdf_order_widget = ListWidget()
 
-        self.layout.addLayout(self.output_folder_layout)
+        self.layout.addLayout(self.source_folder_layout)
+        self.layout.addLayout(self.dest_folder_layout)
+        self.layout.addWidget(QLabel("Drag names to set page structure:"))
+        self.middle_layout.addWidget(self.pdf_order_widget)
+        self.middle_layout.addLayout(self.right_layout)
+        self.layout.addLayout(self.middle_layout)
         self.setLayout(self.layout)
 
     def _get_saveto_path(self):
-        path = QFileDialog.getExistingDirectoryUrl(self, 'Save files to',
-                                                   os.getcwd(), )
+        path = QFileDialog.getExistingDirectory(self,
+                                                'Save files to',
+                                                os.getcwd(), )
         return path
 
     @Slot()
-    def populate_file_name(self):
+    def populate_source_file(self):
         path = self._get_saveto_path()
         if path:
-            self.output_folder.setText(path.toString())
+            self.source_folder.setText(path)
+
+    @Slot()
+    def populate_dest_file(self):
+        path = self._get_saveto_path()
+        if path:
+            self.dest_folder.setText(path)
+
+    @Slot()
+    def load_files(self):
+        source_path = self.source_folder.text()
+        # source_path = "/Users/omendoza/GitHub/Python/UnificarPDFs/resources/polizas/caratulas"
+        pdf_files = [f for f in os.listdir(source_path) if f.endswith('.pdf')]
+        delimiters = "_", " ", "."
+        regex_pattern = '|'.join(map(re.escape, delimiters))
+        splitted_names = [re.split(regex_pattern, x) for x in pdf_files]
+        polizas = list(set([x[0] for x in splitted_names]))
+        poliza_parts = list(set([x[1] for x in splitted_names]))
+        self.pdf_order_widget.addItems(poliza_parts)
+
+        '''
+        nombres_archivo = list(set(map(lambda x: re.split(regex_pattern, for x in ), list(pdf_files))))
+        nombres_archivo = list(set(map(lambda x: x.split(' ')[0], pdf_files)))
+        nombres_archivo = list(set([x.split() for x in pdf_files]))
+        x = '03B8159_03B53_ORIGINAL.pdf'
+        x2 = '03B1212 ORIGINAL_TEX.pdf'
+        splitted = x.split('_|\\ ', -1)
+        splitted2 = re.split(regex_pattern, x2)
+        splitted3 = [x.split('_')[1] for x in pdf_files]
+        '''
+
+    @Slot()
+    def massive_merge(self):
+        # order_list = [self.pdf_order_widget.findItems('', Qt.MatchRegExp)]
+        order_list = [str(self.pdf_order_widget.item(i).text()) for i in range(self.pdf_order_widget.count())]
+        print(order_list)
 
 
-class mainWidget(QWidget):
+class MainWidget(QWidget):
     def __init__(self):
         QWidget.__init__(self)
 
         # Adding the tabs
         self.mainTab = QTabWidget()
-        self.tabSinglePDF = tabSinglePDFWidget()
-        self.tabMassivePDF = tabMassivePDFWidget()
+        self.tabSinglePDF = TabSinglePDFWidget()
+        self.tabMassivePDF = TabMassivePDFWidget()
 
         self.mainTab.addTab(self.tabSinglePDF, "Single PDF")
         self.mainTab.addTab(self.tabMassivePDF, "Massive PDF")
@@ -250,7 +311,7 @@ class MainWindow(QMainWindow):
         QMainWindow.__init__(self)
         self.setWindowTitle("ASSA PDF Merger")
 
-        #Menu
+        # Menu
         self.menu = self.menuBar()
         self.file_menu = self.menu.addMenu("File")
 
@@ -270,8 +331,8 @@ class MainWindow(QMainWindow):
 if __name__ == "__main__":
     # Qt Application
     app = QApplication(sys.argv)
-    app.setStyle('macintosh')
-    widget = mainWidget()
+    app.setStyle('fusion')
+    widget = MainWidget()
     # QMainWindow using QWidget as central widget
     window = MainWindow(widget)
     window.resize(800, 600)
