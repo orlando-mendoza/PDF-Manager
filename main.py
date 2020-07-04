379bdf11c89a7e987ba33ddc1a21797483d5b2fc
Orlando Mendoza <orlandomr27@gmail.com>
2020-07-04 08:37:33 -0600

    PDF Merger finished. It's working software! \o/

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 import os
 import re
 
+from os import path
 from PyPDF2 import PdfFileMerger
 from PySide2.QtCore import Qt, Slot
 from PySide2.QtWidgets import (QApplication, QHBoxLayout, QVBoxLayout, QLabel,
@@ -77,8 +78,6 @@ class OutputField(QLineEdit):
                 self.setText(event.mimeData().urls()[0].toLocalFile())
         else:
             event.ignore()
-
-
 # class outputField
 
 
@@ -184,8 +183,6 @@ class TabSinglePDFWidget(QWidget):
                 self.dialog_message(e)
         else:
             self.dialog_message('There are no files to merge')
-
-
 # class TabSinglePDFWidget
 
 
@@ -217,7 +214,7 @@ class TabMassivePDFWidget(QWidget):
 
         # Creates a button to load files and get the possible order of files
         self.btn_load = Button('Load files')
-        self.btn_load.clicked.connect(self.load_files)
+        self.btn_load.clicked.connect(self.load_files_order)
         self.right_layout.addWidget(self.btn_load)
 
         # Creates a button to merge the files in the order showed by ListWidget
@@ -235,6 +232,13 @@ class TabMassivePDFWidget(QWidget):
         self.middle_layout.addLayout(self.right_layout)
         self.layout.addLayout(self.middle_layout)
         self.setLayout(self.layout)
+
+    def dialogMessage(self, message, icon):
+        dlg = QMessageBox(self)
+        dlg.setWindowTitle('ASSA PDF Manager')
+        dlg.setIcon(icon)
+        dlg.setText(message)
+        dlg.show()
 
     def _get_saveto_path(self):
         path = QFileDialog.getExistingDirectory(self,
@@ -254,34 +258,70 @@ class TabMassivePDFWidget(QWidget):
         if path:
             self.dest_folder.setText(path)
 
-    @Slot()
-    def load_files(self):
-        source_path = self.source_folder.text()
+    def load_files(self, source_path):
+        """ function receives a source path as parameter. Source path has to be previously checked as dir,
+        then loads the file names and split acoording to the regex_pattern
+        :returns file names sepparated by regex_pattern and a list of pdf file names """
         # source_path = "/Users/omendoza/GitHub/Python/UnificarPDFs/resources/polizas/caratulas"
         pdf_files = [f for f in os.listdir(source_path) if f.endswith('.pdf')]
         delimiters = "_", " ", "."
         regex_pattern = '|'.join(map(re.escape, delimiters))
         splitted_names = [re.split(regex_pattern, x) for x in pdf_files]
-        polizas = list(set([x[0] for x in splitted_names]))
-        poliza_parts = list(set([x[1] for x in splitted_names]))
-        self.pdf_order_widget.addItems(poliza_parts)
+        return splitted_names, pdf_files
 
-        '''
-        nombres_archivo = list(set(map(lambda x: re.split(regex_pattern, for x in ), list(pdf_files))))
-        nombres_archivo = list(set(map(lambda x: x.split(' ')[0], pdf_files)))
-        nombres_archivo = list(set([x.split() for x in pdf_files]))
-        x = '03B8159_03B53_ORIGINAL.pdf'
-        x2 = '03B1212 ORIGINAL_TEX.pdf'
-        splitted = x.split('_|\\ ', -1)
-        splitted2 = re.split(regex_pattern, x2)
-        splitted3 = [x.split('_')[1] for x in pdf_files]
-        '''
+    @Slot()
+    def load_files_order(self):
+        if path.isdir(self.source_folder.text()):
+            splitted_names, _ = self.load_files(self.source_folder.text())
+            poliza_parts = list(set([x[1] for x in splitted_names]))
+            self.pdf_order_widget.addItems(poliza_parts)
+        else:
+            self.dialogMessage('Source directory is empty or incorrect. '
+                               'Please correct or choose a source folder!', QMessageBox.Warning)
+
 
     @Slot()
     def massive_merge(self):
-        # order_list = [self.pdf_order_widget.findItems('', Qt.MatchRegExp)]
-        order_list = [str(self.pdf_order_widget.item(i).text()) for i in range(self.pdf_order_widget.count())]
-        print(order_list)
+        if path.isdir(self.source_folder.text()) and path.isdir(self.dest_folder.text()):
+            splitted_names, pdf_files = self.load_files(self.source_folder.text())
+            # order_list = [self.pdf_order_widget.findItems('', Qt.MatchRegExp)]
+            order_list = [str(self.pdf_order_widget.item(i).text())
+                          for i in range(self.pdf_order_widget.count())]
+            print(f'order list: {order_list}')
+            if order_list:
+                polizas = list(set([x[0] for x in splitted_names]))
+                print(f'polizas : {polizas}')
+                try:
+                    for poliza in polizas:
+                        pdf_merger = PdfFileMerger()
+                        for part in order_list:
+                            file_pattern = '_'.join([poliza, part])
+                            file_name = [s for s in pdf_files if file_pattern in s]
+                            print(f'file_name: {file_name}')
+                            if file_name:
+                                fullfile_name = path.join(self.source_folder.text(), file_name[0])
+                                pdf_merger.append(fullfile_name)
+
+                        file_dest = path.join(self.dest_folder.text(), '.'.join([poliza, 'pdf']))
+
+                        # str(str(self.source_folder.text() + poliza + '.pdf'))
+                        print(f'file dest: {file_dest}')
+                        pdf_merger.write(file_dest)
+                        pdf_merger.close()
+                    self.dialogMessage('Massive Merge Completed', QMessageBox.Information)
+                except Exception as e:
+                    self.dialogMessage(str(e))
+            else:
+                self.dialogMessage('Order list is empty, please load files first!',
+                                   QMessageBox.Warning)
+        else:
+            self.dialogMessage('Source or destination directory is not a directory, '
+                               'please correct!', QMessageBox.Warning)
+
+        # try:
+        #
+        # except Exception as e:
+        #     self.dialogMessage(str(e))
 
 
 class MainWidget(QWidget):
